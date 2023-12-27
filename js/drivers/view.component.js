b19a2e436704e05cf6bc8436734a1d214e031f3d
Naim Sulejmani <naim.sulejmani@gmail.com>
2023-12-27 18:05:03 +0100

    View Component for driver

---
+++ js/drivers/view.component.js
@@ -0,0 +1,36 @@
+function ViewDriver() {
+    const self = this;
+
+    const apiDriver = new ApiDriver();
+    self.driver_id = window.location.search.split('=')[1].trim();
+    self.driver = undefined;
+
+    self.find = async function () {
+        self.driver = await apiDriver.findById(self.driver_id);
+        self.refresh('driver');
+    }
+
+    self.find()
+
+    return `<div>
+        <table class="table">
+            <tr>
+                <th>Name</th> 
+                <td>{{self.driver.name}}</td>
+            </tr>
+             <tr>
+                <th>Surname</th> 
+                <td>{{self.driver.surname}}</td>
+            </tr>
+            <tr>
+                <th>Licence Category</th> 
+                <td>{{self.driver.licenceCategory}}</td>
+            </tr>
+              </tr>
+            <tr>
+                <th>Active</th> 
+                <td>{{self.driver.active ? 'YES' : 'NO'}}</td>
+            </tr>
+        </table>
+</div>`
+}
